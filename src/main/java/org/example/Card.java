73603aad02858960cc51b135f5cb9eab3822fd37
Masalkin Artem <as.masalkin@yandex.ru>
2024-04-18 22:14:34 +0300

    fix

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -10,40 +10,6 @@ public class Card {
     private int nominate;
     private String descriptionNominate;
     private String descriptionSuits;
-/*
-    public Card(int nominate, String descriptionNominate, String descriptionSuits) {
-        this.nominate = nominate;
-        this.descriptionNominate = descriptionNominate;
-        this.descriptionSuits = descriptionSuits;
-    }
-/*
-    public int getNominate() {
-        return nominate;
-    }
-
-    public void setNominate(int nominate) {
-        this.nominate = nominate;
-    }
-
-    public String getDescriptionNominate() {
-        return descriptionNominate;
-    }
-
-    public void setDescriptionNominate(String descriptionNominate) {
-        this.descriptionNominate = descriptionNominate;
-    }
-
-    public String getDescriptionSuits() {
-        return descriptionSuits;
-    }
-
-    public void setDescriptionSuits(String descriptionSuits) {
-        this.descriptionSuits = descriptionSuits;
-    }
-
-
-
- */
 
     @Override
     public String toString() {
@@ -53,5 +19,4 @@ public class Card {
                 ", descriptionSuits='" + descriptionSuits + '\'' +
                 '}';
     }
-
 }
